d2dfce67c8d752d43c985b6c48ed05877249896f
YoussefTito80 <youssef.tarek1090@gmail.com>
2026-04-14 12:42:45 +0200

    feat: implement updateProject functionality with image handling

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -1,4 +1,7 @@
 const projectsModel = require('../models/projectsModel');
+const fs = require('fs');
+const path = require('path');
+
 // projectsModel.sync({ alter: true })
 const getAllProjects = async (req, res) => {
     try {
@@ -43,6 +46,67 @@ const addProject = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+const updateProject = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const project = await projectsModel.findByPk(id);
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+    const {
+      name,
+      description,
+      industry,
+      servicesType,
+      client,
+      challenges,
+      solutions,
+      link
+    } = req.body;
+
+    let imagePath = project.Image;
+
+    if (req.file) {
+      if (project.Image) {
+        const oldImagePath = path.join(
+          __dirname,
+          '..',
+          'uploads',
+          path.basename(project.Image)
+        );
+
+        if (fs.existsSync(oldImagePath)) {
+          fs.unlinkSync(oldImagePath);
+        }
+      }
+
+      imagePath = `${process.env.url}/uploads/${req.file.filename}`;
+    }
+
+    await project.update({
+      name,
+      description,
+      Image: imagePath,
+      industry,
+      servicesType,
+      client,
+      challenges,
+      solutions,
+      link
+    });
+
+    res.json({
+      message: "Project updated successfully",
+      project
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 module.exports = {
-    getAllProjects,addProject
+    getAllProjects,addProject,
+    updateProject
 };
--- routes/projectsRoute.js
+++ routes/projectsRoute.js
@@ -6,5 +6,6 @@ const upload = require('../services/upload');
 
 router.get('/', projects.getAllProjects);
 router.post('/add', upload.single('Image'), adminOnly, projects.addProject);
+router.put('/update/:id', upload.single('Image'), adminOnly, projects.updateProject);
 
 module.exports = router;
